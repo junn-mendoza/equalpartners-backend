a0c301efca3d9bfd413d63cce67073ce5ed89434
emeraldjunn <emeraldjunn@gmail.com>
2024-10-04 08:28:59 +0100

    push all

--- app/Http/Resources/RewardGetResource.php
+++ app/Http/Resources/RewardGetResource.php
@@ -18,11 +18,13 @@ class RewardGetResource extends JsonResource
             'id' => $this->id,
             'place_id' => $this->place_id,
             'description' => $this->description,
+            'created_at' => $this->created_at,
             'users' => $this->user_rewards->map(function ($userReward) {
                 return [
                     'id' => $userReward->user->id,
                     'name' => $userReward->user->name,
                     'email' => $userReward->user->email,
+                    'profile' => $userReward->user->profile,
                     'created_at' => $userReward->user->created_at,
                     // Add any other user fields you want to include
                 ];
